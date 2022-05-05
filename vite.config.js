33f1d26d813c6a763da32d5aba3e5bef18f210d5
john-mirage <chaosdynamix@posteo.net>
2022-05-05 12:05:55 +0200

    feat: initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/project-path/",
+  resolve: {
+    alias: {
+      '@images': resolve(__dirname, 'src/images'),
+      '@styles': resolve(__dirname, 'src/styles'),
+    },
+  },
+});
